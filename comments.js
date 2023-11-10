b7f7d249b65c5520956e24fdfbfc3dc45a4ea1c9
Dario Cabascango <hz-hertzio@hotmail.com>
2023-11-10 20:56:58 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,12 @@
-// Create a web server
+// Import module
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+
+// Handle requests
+router.get('/', commentController.comment_list);
+router.post('/', commentController.comment_create); // Assuming you meant to create a comment with this route
+router.get('/:id/delete', commentController.comment_delete);
+
+// Export the router
+module.exports = router;
